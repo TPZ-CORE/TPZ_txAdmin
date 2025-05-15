a05eabcff183b4373f9bc539a3e945483319666a
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-15 21:27:40 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -1496,19 +1496,3 @@ INSERT INTO `society` (`job`, `ledger`) VALUES
 	('wapitishaman', 0),
 	('woodworker', 0);
 /*!40000 ALTER TABLE `society` ENABLE KEYS */;
-
--- Dumping structure for table daisytown.whitelist
-CREATE TABLE IF NOT EXISTS `whitelist` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `identifier` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  `status` tinyint(1) DEFAULT NULL,
-  `firstconnection` tinyint(1) DEFAULT 1,
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE KEY `identifier` (`identifier`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
-
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
