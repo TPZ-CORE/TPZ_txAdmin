f521bba43fb6bb7575533578971c2709180ea1d7
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-30 17:42:12 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -90,6 +90,21 @@ CREATE TABLE IF NOT EXISTS `crafting` (
   PRIMARY KEY (`job`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
 
+-- Dumping structure for table tpzcore.properties
+CREATE TABLE IF NOT EXISTS `properties` (
+  `name` varchar(50) NOT NULL,
+  `identifier` varchar(50) DEFAULT NULL,
+  `charidentifier` int(11) DEFAULT 0,
+  `storage` longtext DEFAULT '{}',
+  `wardrobe` longtext DEFAULT '{}',
+  `ledger` int(11) DEFAULT 0,
+  `keyholders` longtext DEFAULT '[]',
+  `owned` int(1) DEFAULT 0,
+  `duration` int(11) DEFAULT 0,
+  `paid` int(1) DEFAULT 0,
+  PRIMARY KEY (`name`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
+
 -- Dumping structure for table tpzcore.items
 CREATE TABLE IF NOT EXISTS `items` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
