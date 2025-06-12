dd8eafc884bd3799aa178218e4dcad6e96805050
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-06-12 23:26:28 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -32,6 +32,7 @@ CREATE TABLE IF NOT EXISTS `characters` (
   `inactivity_time` int(50) DEFAULT 0,
   `identity_id` varchar(50) DEFAULT NULL,
 	`jailed_until` INT(11) DEFAULT 0,
+	`selected_horse_index` INT(11) DEFAULT 0,
   PRIMARY KEY (`charidentifier`),
   UNIQUE KEY `charidentifier` (`charidentifier`),
   KEY `identifier` (`identifier`),
@@ -104,6 +105,44 @@ CREATE TABLE IF NOT EXISTS `properties` (
   `paid` int(1) DEFAULT 0,
   PRIMARY KEY (`name`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
+
+-- Dumping structure for table tpzcore.horses
+CREATE TABLE IF NOT EXISTS `horses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(40) NOT NULL,
+  `charidentifier` int(11) NOT NULL,
+  `model` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `stats` longtext NOT NULL,
+  `components` longtext NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `age` int(11) NOT NULL DEFAULT 0,
+  `sex` int(11) NOT NULL DEFAULT 0,
+  `training_experience` int(11) DEFAULT 0,
+  `breeding` int(11) DEFAULT 0,
+  `container` int(11) DEFAULT 0,
+  `isdead` int(11) NOT NULL DEFAULT 0,
+  `bought_account` int(11) DEFAULT -1,
+  `date` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Dumping structure for table tpzcore.wagons
+CREATE TABLE IF NOT EXISTS `wagons` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(40) NOT NULL,
+  `charidentifier` int(11) NOT NULL,
+  `model` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `stats` longtext NOT NULL,
+  `components` longtext NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `container` int(11) DEFAULT 0,
+  `broken` int(11) DEFAULT 0,
+  `bought_account` int(11) DEFAULT -1,
+  `date` varchar(50) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- Dumping structure for table tpzcore.items
 CREATE TABLE IF NOT EXISTS `items` (
