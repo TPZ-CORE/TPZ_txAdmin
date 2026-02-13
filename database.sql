6419f3e72bc2ff7406da9872e77676dc7d148bfb
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2026-02-13 12:51:42 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -48,6 +48,31 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`identifier`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
 
+
+CREATE TABLE IF NOT EXISTS `admin_history` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(40) NOT NULL,
+  `steamname` varchar(50) NOT NULL,
+  `action` longtext NOT NULL,
+  `date` varchar(50) NOT NULL,
+  `timestamp` int(11) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
+
+CREATE TABLE IF NOT EXISTS `admin_tickets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(40) NOT NULL,
+  `charidentifier` int(11) NOT NULL,
+  `steamname` varchar(50) NOT NULL,
+  `fullname` varchar(50) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `message` longtext NOT NULL,
+  `exclamation` int(11) DEFAULT 0,
+  `date` varchar(50) NOT NULL,
+  `timestamp` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
+
 -- Dumping structure for table tpzcore.subscriptions
 CREATE TABLE IF NOT EXISTS `subscriptions` (
   `identifier` varchar(50) NOT NULL,
@@ -1467,6 +1492,7 @@ INSERT INTO `items` (`id`, `item`, `label`, `weight`, `remove`, `type`, `descrip
 	(1100, 'yeast', 'Yeast', 0, 0, 'item', 'nothing', 'none', 1, 1, 0),
 	(1101, 'yogurt', 'Yogurt', 0, 0, 'item', 'nothing', 'none', 1, 1, 0);
 /*!40000 ALTER TABLE `items` ENABLE KEYS */;
+
 
 
 
